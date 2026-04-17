2b267cf0f3c29edc8608a2e29b6ccf97d5ed5947
Nazimcp-git <78524092+Nazimcp-git@users.noreply.github.com>
2026-04-17 17:05:08 +0530

    Update Firebase configuration details

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 
   // Your Firebase config
   var firebaseConfig = {
-    apiKey: "AIzaSyBXJ_RnfjUDi7qPDATWVnS5lSFw6jVRYgo",
-  authDomain: "shopping-e284c.firebaseapp.com",
-  databaseURL: "https://shopping-e284c-default-rtdb.firebaseio.com",
-  projectId: "shopping-e284c",
-  storageBucket: "shopping-e284c.appspot.com",
-  messagingSenderId: "248274428739",
-  appId: "1:248274428739:web:fc30dd9eb1ef83f610c5f6",
-  measurementId: "G-ZXZCK9BW7T"
+    apiKey: "AIzaSyB-7xg8rHTrZNAMQ1Rx7w6mjYqWJwQrIn4",
+  authDomain: "ratt-7c9ab.firebaseapp.com",
+  databaseURL: "https://ratt-7c9ab-default-rtdb.firebaseio.com",
+  projectId: "ratt-7c9ab",
+  storageBucket: "ratt-7c9ab.appspot.com",
+  messagingSenderId: "582900835779",
+  appId: "1:582900835779:web:4621839b23c35faf39c976",
+  measurementId: "G-GE6L76ZWGG"
   };
   firebase.initializeApp(firebaseConfig);
   var db = firebase.firestore();
